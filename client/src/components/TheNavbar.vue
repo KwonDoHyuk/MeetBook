e49e753950fc186298704a5f5edb90a67bb4153d
조성한 <toor21@naver.com>
2022-01-19 11:20:05 +0900

    feat: create navbar

--- client/src/components/TheNavbar.vue
+++ client/src/components/TheNavbar.vue
@@ -1,22 +1,21 @@
 <template>
   <v-app-bar app color="#FAF6EA">
     <v-toolbar-title>Meetbook</v-toolbar-title>
-    <v-btn text plain
-      to="/conf/list"
-    >
+    <v-btn text plain to="/conf/list">
       <span class="mr-2">모임</span>
     </v-btn>
+
     <v-spacer></v-spacer>
 
     <!-- 검색 -->
     <v-text-field
-      solo
+      solo dense flat
       placeholder="검색"
       append-outer-icon="mdi-magnify"
       hide-details="true"
       style="max-width: 450px;"
-
-      :model="keyword"
+      v-model="keyword"
+      @keyup.enter="searchKeyword"
       >
     </v-text-field>
 
@@ -74,8 +73,13 @@
 
       <v-list>
         <v-list-item
-          v-for="(item, index) in profileMenuItems" :key="index">
+          v-for="(item, index) in profileMenuItems" :key="index" :to="item.to">
           <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+
+        <!-- router 대신 다른 작업을 해야 하는 로그아웃만 따로 떼어서 작성 -->
+        <v-list-item @click="logout">
+          <v-list-item-title>로그아웃</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-menu>
@@ -84,6 +88,8 @@
 </template>
 
 <script>
+// import axios from 'axios'
+import { mapGetters } from 'vuex'
 export default {
   name: "navbar",
   data: function () {
@@ -92,14 +98,34 @@ export default {
       profileMenuItems: [
         { title: '내 프로필', to:'/profile',},
         { title: '계정 설정', to:'/accounts/settings',},
-        { title: '로그아웃', to:'/logout',},
       ],
       keyword: '',
     }
   },
 
   methods: {
+    // 로그아웃
+    logout: function () {
+      // localStorage.removeItem('토큰')
+      // this.$$store.dispatch('로그아웃 관련 Vuex Method')
+      // this.$router.push({name: 'Home'}) // 홈으로 보내기
+      console.log('로그아웃')
+    },
 
+    // 검색
+    searchKeyword: function () {
+      let q = this.keyword
+      console.log(q)
+      // axios({
+      //   method: 'get',
+      //   url: ''
+      // })
+    }
+  },
+
+  computed: {
+    // 로그인 여부 확인
+    ...mapGetters(['isLogin',])
   },
 }
 </script>
