314f25764f30892924cd18792ca2c1c28d057090
조성한 <toor21@naver.com>
2022-01-26 15:27:43 +0900

    feat: add conference screen

--- client/src/views/conference/Mainscreen.vue
+++ client/src/views/conference/Mainscreen.vue
@@ -11,16 +11,36 @@
       <!-- 주 진행자 화면 -->
       <div>
         <v-card class="ma-4" min-height="480px">주 진행자 화면</v-card>
-        
       </div>
       <!-- 기타 청취자 화면 -->
-      <v-sheet class="mx-auto my-5" elevation="8">
-        <v-slide-group class="pa-4" v-model="participants" active-class="success" show-arrows>
-          <v-slide-item>
-            <v-card class="ma-4">사용자 1</v-card>
-          </v-slide-item>
-          <v-slide-item>
-            <v-card class="ma-4">사용자 2</v-card>
+      <v-sheet class="my-4">
+        <v-slide-group class="pa-1" active-class="success" show-arrows>
+          <v-slide-item v-for="n in 20" :key="n" class="ma-2">
+            <div>
+              <v-menu offset-y>
+                <template v-slot:activator="{attrs, on}">
+                  <v-card v-bind="attrs" v-on="on">
+                    사용자 {{ n }}
+                  </v-card>
+                </template>
+                <v-list>
+                  <v-list-item link @click="onOffcamera(n)">
+                    화면 끄기
+                  </v-list-item>
+                  <v-list-item link @click="onMute(n)">
+                    음소거
+                  </v-list-item>
+                  <v-divider></v-divider>
+                  <v-list-item link @click="onKick(n)">
+                    강퇴
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+
+            </div>
+
+
+
           </v-slide-item>
         </v-slide-group>
       </v-sheet>
@@ -34,29 +54,18 @@
       <div class="d-flex flex-column">
         <!-- 채팅창 -->
         <v-card outlined class="overflow-y-auto ma-4" min-height="560px" max-height="560px">
-          <p>사용자 (시간): <br>채팅내용</p>
-          <p>사용자 (시간): <br>채팅내용</p>
-          <p>사용자 (시간): <br>채팅내용</p>
-          <p>사용자 (시간): <br>채팅내용</p>
-          <p>사용자 (시간): <br>채팅내용</p>
-          <p>사용자 (시간): <br>채팅내용</p>
-          <p>사용자 (시간): <br>채팅내용</p>
-          <p>사용자 (시간): <br>채팅내용</p>
-          <p>사용자 (시간): <br>채팅내용</p>
-          <p>사용자 (시간): <br>채팅내용</p>
-          <p>사용자 (시간): <br>채팅내용</p>
-          <p>사용자 (시간): <br>채팅내용</p>
-          <p>사용자 (시간): <br>채팅내용</p>
-          <p>사용자 (시간): <br>채팅내용</p>
+          <p v-for="(log, idx) in chatlog" :key="idx">
+            {{ log.user }} ({{ log.timestamp }}): <br>{{ log.content }}
+          </p>
           
         </v-card>
         <!-- 채팅 폼 -->
         <div class="d-flex flex-row">
           <div class="col-8">
-          <v-textarea no-resize></v-textarea>
+          <v-textarea no-resize v-model="inputChat" @keyup.enter="onInputChat"></v-textarea>
           </div>
           <div class="col-4">
-          <v-btn block>보내기</v-btn>
+          <v-btn block @click="onInputChat">보내기</v-btn>
           </div>
         </div>
       </div>
@@ -66,7 +75,7 @@
         <v-btn>
           환경설정
         </v-btn>
-        <v-btn color="accent">
+        <v-btn color="accent" @click="onExit">
           나가기
         </v-btn>
       </div>
@@ -91,7 +100,51 @@ export default {
   data: function () {
     return {
       participants: null,
+      showMenu: false,
+      inputChat: '',
+
+      chatlog: [
+        {user: '사용자 1', timestamp: '2011-10-05T14:48:00', content: '우리 개발 화이팅'},
+        {user: '사용자 1', timestamp: '2011-10-05T14:48:00', content: '백엔드 화이팅'},
+        {user: '사용자 1', timestamp: '2011-10-05T14:48:00', content: '백엔드 화이팅'},
+        {user: '사용자 1', timestamp: '2011-10-05T14:48:00', content: '백엔드 화이팅'},
+        {user: '사용자 1', timestamp: '2011-10-05T14:48:00', content: '백엔드 화이팅'},
+        {user: '사용자 1', timestamp: '2011-10-05T14:48:00', content: '백엔드 화이팅'},
+        {user: '사용자 1', timestamp: '2011-10-05T14:48:00', content: '백엔드 화이팅'},
+        {user: '사용자 1', timestamp: '2011-10-05T14:48:00', content: '프론트엔드 화이팅'},
+        {user: '사용자 1', timestamp: '2011-10-05T14:48:00', content: '프론트엔드 화이팅'},
+        {user: '사용자 1', timestamp: '2011-10-05T14:48:00', content: '프론트엔드 화이팅'},
+        {user: '사용자 1', timestamp: '2011-10-05T14:48:00', content: '프론트엔드 화이팅'},
+        {user: '사용자 1', timestamp: '2011-10-05T14:48:00', content: '프론트엔드 화이팅'},
+        {user: '사용자 1', timestamp: '2011-10-05T14:48:00', content: '프론트엔드 화이팅'},
+      ],
     }
+  },
+
+  methods: {
+    onOffcamera: function (userid) {
+      console.log('offcam' + userid)
+    },
+
+    onMute: function (userid) {
+      console.log('Mute' + userid)
+    },
+
+    onKick: function (userid) {
+      console.log('kick' + userid)
+    },
+
+    onExit: function () {
+      console.log('exit')
+    },
+
+    onInputChat: function () {
+      if (this.inputChat != '') {
+        console.log('input:' + this.inputChat)
+        this.inputChat = ''
+      }
+    },
+
   },
 
 }
