151e3eddfa7ca1777ad581397a5b653d2184f13f
euijinbang <euijin.bang@gmail.com>
2022-02-25 03:34:30 +0900

    jenkins test

--- client/src/components/TheFooter.vue
+++ client/src/components/TheFooter.vue
@@ -3,8 +3,8 @@
       <footer>
         <div class="container">
           <div class="item d-flex justify-center">
-            <p class="">Meetbook © 2022</p><br>
-            <p class="">ver 1.0.0</p>
+            <p class="">Meetbook © 2022</p>
+            <p class="">Ver 1.0.0</p>
           </div>
         </div>
       </footer>
